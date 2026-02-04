a58b80def7d2724bead7a5c5546e70dec8cde67d
Rashmi0607 <132034080+Rashmi0607@users.noreply.github.com>
2026-02-04 14:43:03 +0530

    fixed app file

--- vendor/src/components/login.jsx
+++ vendor/src/components/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Image from "../assets/Login-page.jpg";
+import Image from "../assets/Login-page.png";
 
 export default function Login() {
   const [email, setEmail] = useState("");
